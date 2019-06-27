d3ca92fa06f6a68f4e38e2a39417618d8441a313
FayeGitHub <ThinkPad@Faye>
2019-06-27 13:37:14 +0800

    测试解决中文乱码

--- src/main/java/com/company/test/controller/HelloWorldController.java
+++ src/main/java/com/company/test/controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
 	@RequestMapping("/helloworld.do")
 	public String helloworld(HttpServletRequest request, HttpServletResponse response){
 		HttpSession session = request.getSession();
-		session.setAttribute("message","ÄãºÃ£¬ÊÀ½ç£¡");
+		session.setAttribute("message","ä½ å¥½ï¼Œä¸–ç•Œ");
 		return "helloworld";
 	}
 
