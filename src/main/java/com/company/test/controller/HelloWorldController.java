c2f7a37a9d6395ebd68fa894e7fa22168adecf8e
FayeGitHub <ThinkPad@Faye>
2019-06-27 13:20:03 +0800

    空工程

---
+++ src/main/java/com/company/test/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.company.test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/helloworld.do")
+	public String helloworld(HttpServletRequest request, HttpServletResponse response){
+		HttpSession session = request.getSession();
+		session.setAttribute("message","ÄãºÃ£¬ÊÀ½ç£¡");
+		return "helloworld";
+	}
+
+}
